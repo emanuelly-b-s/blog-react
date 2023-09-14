e73b2ece23849d8a3242a3176b468b50887dbc6e
manu <emanuellybs14@gmail.com>
2023-09-14 14:45:47 -0300

    \

--- src/pages/navbar/index.js
+++ src/pages/navbar/index.js
@@ -9,25 +9,16 @@ import styles from './styles.module.scss';
 export default function NavBar() {
     return (
 
-        <Navbar expand="lg" className={styles.nav}>
-                <div className={styles.supergraphic} />
+        <Nav className={styles.nav}>
+            <div className={styles.supergraphic} />
+            <div className={styles.subnavbar}>
                 <div className={styles.logo} />
-            <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-            >
-                
-            <Navbar.Toggle aria-controls="navbarScroll" />
-
                 <div className={styles.links}>
                     <Link to='/home' className={styles.links__link}>Home</Link>
                     <Link to='/add' className={styles.links__link}>Adicionar</Link>
                 </div>
-
-            </Nav>
-
-        </Navbar>
+            </div>
+        </Nav>
 
     );
 }
