d5416fbe4459ecf321d6455e7820f6626135dd6f
Emanuelly Bueno <emanuellybs14@gmail.com>
2023-09-15 11:27:48 -0300

    alterando endpoint register

--- back/src/controller/AuthController.js
+++ back/src/controller/AuthController.js
@@ -7,31 +7,52 @@ require('dotenv').config();
 
 class AuthController {
     static async register(req, res) {
-        const { name, email, password } = req.body;
-        // const json = CryptoJS.AES.decrypt(jsonCrypto, 'teste').toString(CryptoJS.enc.Utf8);
-        
-        // const { name, email, password } = JSON.parse(json);
+        var bytes = CryptoJS.AES.decrypt(req.body.jsonCrypt, process.env.SECRET);
 
-        if (!name || !email || !password)
-            return res.status(400).send({ message: "name or email or password not provider" });
+        const decryptd = bytes.toString(CryptoJS.enc.Utf8);
+
+        const json = JSON.parse(decryptd);
+
+        const { name, birth, email, password, confirmPassword } = json;
+
+        if (!name)
+            return res.status(400).json({ message: "O nome é obrigatório" });
+        if (!email)
+            return res.status(400).json({ message: "O e-mail é obrigatório" });
+        if (!password)
+            return res.status(400).json({ message: "A senha é obrigatória" });
+        if (password != confirmPassword)
+            return res.status(400).json({ message: "As senhas não conferem" });
+        const userExist = await User.findOne({ email: email });
+        if (userExist)
+            return res.status(422).json({ message: "insira outro e-mail" });
+
+        const passwordCrypt = CryptoJS.AES.encrypt(password, process.env.SECRET).toString();
+
+        const author = new Author({
+            name,
+            email,
+            birth,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+            removedAt: null,
+        })
+
+        const user = new User({
+            login: email,
+            author,
+            email,
+            password: passwordCrypt,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+            removedAt: null,
+        });
 
         try {
-            // const user = await User.findOne({ email })
-            if (await User.findOne({ email }))
-                return res.status(400).send({ message: "aa" });
-
-            const passwordCrypt = CryptoJS.AES.encrypt(password, 'teste').toString();
-
-            const newUser = new User({
-                name: name,
-                email: email,
-                password: passwordCrypt
-            }) ;
-
-            await newUser.save();
-            return res.status(201).send({ message: "user created" });
+            await User.create(user);
+            res.status(201).send({ message: "Usuário cadastrado com sucesso" });
         } catch (error) {
-            return res.status(500).send({ message: "something faild" });
+            return res.status(500).send({ message: "Something failed", data: error.message })
         }
     }
 
